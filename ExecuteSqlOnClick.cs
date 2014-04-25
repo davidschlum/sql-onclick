02ad571f456fe8b27066f7b05828004f02d64d47
davidschlum <david@oldtownit.com>
2014-04-25 09:20:50 -0500

    Adding comments to help with understanding

--- ExecuteSqlOnClick.cs
+++ ExecuteSqlOnClick.cs
@@ -15,23 +15,28 @@ namespace OTI.AUGTech2014.Extensions
         public void Initialize(PageClass page, Control control, string sqlScript)
         {
             Button btn = (Button)control;
+            //instantiate onClick event args to pass page and sql script to Click event
             SqlOnClickEventArgs eventArgs = new SqlOnClickEventArgs(){
                 Page = page,
                 SqlScript = sqlScript
             };
-
+            //wire up Click event and use delegate in order to pass custom event args
             btn.Click += delegate(object sender, EventArgs e)
             {
                 btn_Click(sender, eventArgs);
             };
+            //Make sure the user can't click the button more than once
             UtilityFunctions.DoubleSubmitPrevention(page, btn);
         }
 
         private void btn_Click(object sender, SqlOnClickEventArgs e)
         {
+            //parse any values in the sql script
             string _sqlScript = DataUtils.ParseValues(e.Page.oFacadeObject, e.SqlScript);
-            DataUtils.ExecuteSql(_sqlScript);
+            //execute the script that has been parsed
+            DataUtils.ExecuteSql(_sqlScript); //any errors get set to Config.LastError
             string message;
+            //Check for error and do something for either case
             if (!UtilityFunctions.ER())
             {
                 message = "Success";
